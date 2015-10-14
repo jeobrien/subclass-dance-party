e4c3abda7dff4b7d7196a10808e5a17b2811389d
jeobrien <j.e.obrien28@gmail.com>
2015-10-14 15:17:54 -0700

    debugged problems with oldStep and "this" references causing infinite loops

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,8 +4,6 @@
 //   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
 //   // so we must keep a copy of the old version of this function
 
-//   var oldStep = blinkyDancer.step;
-
 //   blinkyDancer.step = function() {
 //     // call the old version of step at the beginning of any call to this new version of step
 //     oldStep();
@@ -21,11 +19,13 @@
 //============PSEUDOCLASSICAL
 var makeBlinkyDancer = function (top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+  // this.$node = $('<span class="dancer"></span>');
+  // this.step(timeBetweenSteps);
+  // this.setPosition(top, left);
 };
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
 makeBlinkyDancer.prototype.step = function () {
-  this.oldStep();
+  makeDancer.prototype.step.call(this);
   this.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -35,9 +35,10 @@ var makeDancer = function (top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this.step(timeBetweenSteps);
   this.setPosition(top, left);
+  this.timeBetweenSteps = timeBetweenSteps;
 };
-makeDancer.prototype.step = function (timeBetweenSteps) {
-  setTimeout(this.step, timeBetweenSteps);
+makeDancer.prototype.step = function () {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 makeDancer.prototype.setPosition = function (top, left) {
   this.styleSettings = {
